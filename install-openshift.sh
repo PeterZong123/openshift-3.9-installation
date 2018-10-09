70a28a65e1ad992b52515d6af02e8089f098ed3a
PeterZong123 <34564650+PeterZong123@users.noreply.github.com>
2018-10-09 17:05:16 +0800

    Remove useless script code

--- install-openshift.sh
+++ install-openshift.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-## see: https://youtu.be/aqXSbDZggK4
-
 ## Default variables to use
 export INTERACTIVE=${INTERACTIVE:="true"}
-export PVS=${INTERACTIVE:="true"}
 export DOMAIN=${DOMAIN:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}').nip.io"}
 export USERNAME=${USERNAME:="$(whoami)"}
 export PASSWORD=${PASSWORD:=password}
-export VERSION=${VERSION:="3.9.0"}
 export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/PeterZong123/openshift-3.9-installation/master"}
 export IP=${IP:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"}
 export API_PORT=${API_PORT:="8443"}
@@ -30,10 +26,6 @@ if [ "$INTERACTIVE" = "true" ]; then
 		export PASSWORD="$choice";
 	fi
 
-	read -rp "OpenShift Version: ($VERSION): " choice;
-	if [ "$choice" != "" ] ; then
-		export VERSION="$choice";
-	fi
 	read -rp "IP: ($IP): " choice;
 	if [ "$choice" != "" ] ; then
 		export IP="$choice";
@@ -53,7 +45,6 @@ echo "* Your domain is $DOMAIN "
 echo "* Your IP is $IP "
 echo "* Your username is $USERNAME "
 echo "* Your password is $PASSWORD "
-echo "* OpenShift version: $VERSION "
 echo "******"
 
 # install updates
@@ -93,23 +84,6 @@ cat <<EOD > /etc/hosts
 ${IP}		$(hostname) console console.${DOMAIN}  
 EOD
 
-if [ -z $DISK ]; then 
-	echo "Not setting the Docker storage."
-else
-	cp /etc/sysconfig/docker-storage-setup /etc/sysconfig/docker-storage-setup.bk
-
-	echo DEVS=$DISK > /etc/sysconfig/docker-storage-setup
-	echo VG=DOCKER >> /etc/sysconfig/docker-storage-setup
-	echo SETUP_LVM_THIN_POOL=yes >> /etc/sysconfig/docker-storage-setup
-	echo DATA_SIZE="100%FREE" >> /etc/sysconfig/docker-storage-setup
-
-	systemctl stop docker
-
-	rm -rf /var/lib/docker
-	wipefs --all $DISK
-	docker-storage-setup
-fi
-
 systemctl restart docker
 systemctl enable docker
 
@@ -135,19 +109,6 @@ fi
 curl -o inventory.download $SCRIPT_REPO/inventory.ini
 envsubst < inventory.download > inventory.ini
 
-# add proxy in inventory.ini if proxy variables are set
-if [ ! -z "${HTTPS_PROXY:-${https_proxy:-${HTTP_PROXY:-${http_proxy}}}}" ]; then
-	echo >> inventory.ini
-	echo "openshift_http_proxy=\"${HTTP_PROXY:-${http_proxy:-${HTTPS_PROXY:-${https_proxy}}}}\"" >> inventory.ini
-	echo "openshift_https_proxy=\"${HTTPS_PROXY:-${https_proxy:-${HTTP_PROXY:-${http_proxy}}}}\"" >> inventory.ini
-	if [ ! -z "${NO_PROXY:-${no_proxy}}" ]; then
-		__no_proxy="${NO_PROXY:-${no_proxy}},${IP},.${DOMAIN}"
-	else
-		__no_proxy="${IP},.${DOMAIN}"
-	fi
-	echo "openshift_no_proxy=\"${__no_proxy}\"" >> inventory.ini
-fi
-
 ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
 ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
 
@@ -155,22 +116,6 @@ htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
 
 systemctl restart origin-master-api
-
-if [ "$PVS" = "true" ]; then
-	for i in `seq 1 200`;
-	do
-		DIRNAME="vol$i"
-		mkdir -p /mnt/data/$DIRNAME 
-		chcon -Rt svirt_sandbox_file_t /mnt/data/$DIRNAME
-		chmod 777 /mnt/data/$DIRNAME
-		
-		sed "s/name: vol/name: vol$i/g" vol.yaml > oc_vol.yaml
-		sed -i "s/path: \/mnt\/data\/vol/path: \/mnt\/data\/vol$i/g" oc_vol.yaml
-		oc create -f oc_vol.yaml
-		echo "created volume $i"
-	done
-	rm oc_vol.yaml
-fi
 
 echo "******"
 echo "* Your console is https://console.$DOMAIN:$API_PORT"
