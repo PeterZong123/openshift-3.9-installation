382311ba44691d91ed279c4c1a778334ad5fbd26
PeterZong123 <34564650+PeterZong123@users.noreply.github.com>
2018-10-17 16:05:34 +0800

    Update vm-prerequisites.sh

--- vm-prerequisites.sh
+++ vm-prerequisites.sh
@@ -2,43 +2,13 @@
 
 # default variables to use
 export INTERACTIVE=${INTERACTIVE:="true"}
-export DOMAIN=${DOMAIN:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}').nip.io"}
-export IP=${IP:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"}
-export API_PORT=${API_PORT:="8443"}
 export ANSIBLE_PLAYBOOKS_REPO=${ANSIBLE_PLAYBOOKS_REPO:="https://github.com/PeterZong123/openshift-ansible.git"}
-export METRICS=${METRICS:="False"}
-export LOGGING=${LOGGING:="False"}
 
 # make the script interactive to set the variables
 if [ "$INTERACTIVE" = "true" ]; then
-	read -rp "Domain to use: ($DOMAIN): " choice;
-	if [ "$choice" != "" ] ; then
-		export DOMAIN="$choice";
-	fi
-
-	read -rp "IP: ($IP): " choice;
-	if [ "$choice" != "" ] ; then
-		export IP="$choice";
-	fi
-
-	read -rp "API Port: ($API_PORT): " choice;
-	if [ "$choice" != "" ] ; then
-		export API_PORT="$choice";
-	fi 
-
 	read -rp "Ansible Playbooks Repo: ($ANSIBLE_PLAYBOOKS_REPO): " choice;
 	if [ "$choice" != "" ] ; then
 		export ANSIBLE_PLAYBOOKS_REPO="$choice";
-	fi 
-
-	read -rp "Enable Metrics: ($METRICS): " choice;
-	if [ "$choice" != "" ] ; then
-		export METRICS="$choice";
-	fi 
-
-	read -rp "Enable Logging: ($LOGGING): " choice;
-	if [ "$choice" != "" ] ; then
-		export LOGGING="$choice";
 	fi 
 
 	echo
@@ -84,23 +54,9 @@ cd ..
 
 cd openshift-ansible && git fetch && git checkout release-3.9 && cd ..
 
-# modify hosts
-cat <<EOD > /etc/hosts
-127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
-::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-${IP}		$(hostname) console console.${DOMAIN}  
-EOD
-
 # restart docker
 systemctl restart docker
 systemctl enable docker
-
-# generate ssh key
-if [ ! -f ~/.ssh/id_rsa ]; then
-	ssh-keygen -q -f ~/.ssh/id_rsa -N ""
-	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-	ssh -o StrictHostKeyChecking=no root@$IP "pwd" < /dev/null
-fi
 
 # pull images
 docker image pull cockpit/kubernetes:latest
@@ -118,59 +74,3 @@ docker image pull openshift/origin-service-catalog:3.9
 docker image pull openshift/origin-metrics-cassandra:v3.9
 docker image pull openshift/origin-metrics-hawkular-metrics:v3.9
 docker image pull openshift/origin-metrics-heapster:v3.9
-
-# create inventory.ini
-cat > inventory.ini <<EOF
-[OSEv3:children]
-masters
-nodes
-etcd
-
-[masters]
-${IP} openshift_ip=${IP} openshift_schedulable=true 
-
-[etcd]
-${IP} openshift_ip=${IP}
-
-[nodes]
-${IP} openshift_ip=${IP} openshift_schedulable=true openshift_node_labels="{'region': 'infra', 'zone': 'default'}"
-
-[OSEv3:vars]
-ansible_ssh_user=root
-enable_excluders=False
-enable_docker_excluder=False
-ansible_service_broker_install=False
-
-containerized=True
-os_sdn_network_plugin_name='redhat/openshift-ovs-multitenant'
-openshift_disable_check=disk_availability,docker_storage,memory_availability,docker_image_availability
-
-openshift_node_kubelet_args={'pods-per-core': ['50']}
-
-deployment_type=origin
-openshift_deployment_type=origin
-
-
-openshift_release=v3.9
-openshift_pkg_version=-3.9.0
-openshift_image_tag=v3.9.0
-openshift_service_catalog_image_version=v3.9
-template_service_broker_image_version=v3.9
-template_service_broker_selector={"region":"infra"}
-openshift_metrics_image_version="v3.9"
-openshift_logging_image_version="v3.9.0"
-openshift_logging_elasticsearch_proxy_image_version="v1.0.0"
-logging_elasticsearch_rollout_override=false
-osm_use_cockpit=false
-openshift_install_examples=true
-
-openshift_metrics_install_metrics=${METRICS}
-openshift_logging_install_logging=${LOGGING}
-
-openshift_master_identity_providers=[{'name': 'htpasswd_auth', 'login': 'true', 'challenge': 'true', 'kind': 'HTPasswdPasswordIdentityProvider', 'filename': '/etc/origin/master/htpasswd'}]
-
-openshift_public_hostname=console.${DOMAIN}
-openshift_master_default_subdomain=apps.${DOMAIN}
-openshift_master_api_port=${API_PORT}
-openshift_master_console_port=${API_PORT}
-EOF
